3b8aef84fab5be9ea992fd3b08421f706f93017d
Peter Henell <mail@peterhenell.se>
2017-12-12 16:19:07 +0100

    division by zero protection

--- Performance/Session Active Query and Query Plan.sql
+++ Performance/Session Active Query and Query Plan.sql
@@ -18,15 +18,15 @@ SELECT
    -- , nest_level
     , [trancount] = trans.open_transaction_count
     , blocking_session_id
-    , [Paralellism] = 1.0 * req.cpu_time / (req.total_elapsed_time * 1.0 )
+    , [Paralellism] = 1.0 * req.cpu_time / (NULLIF(req.total_elapsed_time, 0) * 1.0 )
     , req.wait_time, req.wait_type
 FROM sys.dm_exec_requests req
 left outer join sys.dm_exec_sessions ses
     on req.session_id = ses.session_id
-CROSS APPLY sys.dm_exec_query_plan(plan_handle)  qplan
+outer APPLY sys.dm_exec_query_plan(plan_handle)  qplan
 LEFT outer JOIN sys.dm_tran_session_transactions trans
     ON trans.session_id = req.session_id
-CROSS  APPLY sys.dm_exec_text_query_plan(plan_handle,
+outer  APPLY sys.dm_exec_text_query_plan(plan_handle,
                                             req.statement_start_offset,
                                             req.statement_end_offset) qp
 WHERE req.session_id <> @@SPID-- and req.session_id = 86
